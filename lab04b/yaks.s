6da5ca1ba9e0eeea69eb7ee61feb37bef6cbe025
Clint Frandsen <clint.frandsen@gmail.com>
2019-10-14 01:24:29 -0600

    Update yaks.s

--- lab04b/yaks.s
+++ lab04b/yaks.s
@@ -9,19 +9,30 @@ YKExitMutex:                    ; Enables Interrupts
 	sti	                          ; set interrupt flag 
 	ret
 
+YKDispatcherNSave:   		; Dispatcher that saves to mem/stack
+	cli
+	push bp
+	mov bp, sp
 
-YKDispatcher:   		; Dispatcher that saves to mem/stack
+	mov sp, word[bp+4]			; getting first arg
+	mov ss, word[bp+6]			; getting second arg	
+	
+	pop ES
+	pop DS
+	pop DI
+	pop SI
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	iret		
+
+YKDispatcherSave:   		; Dispatcher that saves to mem/stack
 	cli
 	push bp
 	mov bp, sp
 	
-						
-	mov ax, word [bp+4]			; getting first arg(the save flag)
-	test ax, ax				; if (flag == 0)
-	jz	restore				; If zero, skip the save
-	
-	
-store:
 	mov AX, [bp+2]	; The return address.
 	pop bp
 	add sp, 2
@@ -44,20 +55,14 @@ store:
 	mov bp, sp
 	add bp, 20
 
-	mov si, word [bp+6] 			; getting second arg (sp)
+	mov si, word [bp+4] 			; getting first arg (sp)
 	mov word [si], sp
 	
-	mov si, word [bp+8]			; getting third arg (ss)
+	mov si, word [bp+6]			; getting second arg (ss)
 	mov word [si], ss
 
-	mov sp, word[bp+10]			; getting forth arg
-	mov ss, word[bp+12]			; getting fifth arg	
-	
-	
-
-restore:
-	mov sp, word[bp+10]			; getting forth arg
-	mov ss, word[bp+12]			; getting fifth arg	
+	mov sp, word[bp+8]			; getting third arg
+	mov ss, word[bp+10]			; getting forth arg	
 	
 	pop ES
 	pop DS
