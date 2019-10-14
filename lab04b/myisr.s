9a34299e46e3fa75c15a909c59225df5f4eed626
Clint Frandsen <clint.frandsen@gmail.com>
2019-10-13 22:28:46 -0600

    Update myisr.s

--- lab04b/myisr.s
+++ lab04b/myisr.s
@@ -8,87 +8,176 @@
 ;7 Execute the iret instruction. This restores in one atomic operation the values for IP, CS, and the flags, which were automatically -
 ;	; - saved on the stack by the processor when the interrupt occurred. This effectively restores execution to the point where the interrupt occurred.
 
-RESET:
-	call c_reset_handler
-	pop word[bp+20]
 
-KEY:
 
-	; save reg
+isr_reset:
+	; save context
 	push ax
 	push bx
 	push cx
 	push dx
-	
+	push bp
 	push si
 	push di
-	push bp
-	push es 
 	push ds
+	push es
+	; Here we test to see if we are the lowest-level interrupt.
+	; If we are, we need to save the task's stack that we interrupted
+	mov ax, [YKISRCallDepth]
+	test ax, ax
+	jnz isr_reset_not_lowest_interrupt
+
+	; Save the SP of the task we interrupted
+	mov bx, [YKRdyList]
+	mov [bx], sp
+
+isr_reset_not_lowest_interrupt:
+
+	call YKEnterISR
+	; enable interrupts for higher priority IRQ
+	sti	
+
+	; run interrupt handler
 	
-	; enable interrupts for higher priority IRQs ? 
-	sti
+	; disable interrupts
+	; sent EOI to PIC
+	; restore context
+
+	; But we don't have to do any of that, because we ARE the highest-priority interrupt
+	; And it will end the program. So no saving context, no enabling interrupts, 
+	; and no restoring context.
+	call c_isr_reset
 	
-	; run inte
-	call c_key_handler
-	
-	; disable interrupts ?
+
 	cli
 	
-	; send eoi to pic
-	mov al, 0x20
-	out 0x20, al
+	mov	al, 0x20
+	out 	0x20, al
+
+		
+	call YKExitISR
 	
-	;restore reg
-	pop ds
 	pop es
-	pop bp
+	pop ds
 	pop di
 	pop si
+	pop bp
 	pop dx
 	pop cx
 	pop bx
-	pop ax
+	pop ax	
 	
-	; execute iret
-	iret 
+	iret	; This should not even happen.
 
-TICK:
-	; save reg
+
+
+isr_keypress:
+		; Save context
 	push ax
 	push bx
 	push cx
 	push dx
-	
+	push bp
 	push si
 	push di
-	push bp
-	push es 
 	push ds
-	
-	; enable interrupts for higher priority IRQs ? 
+	push es
+
+	; Here we test to see if we are the lowest-level interrupt.
+	; If we are, we need to save the task's stack that we interrupted
+	mov ax, [YKISRCallDepth]
+	test ax, ax
+	jnz isr_keypress_not_lowest_interrupt
+
+	; Save the SP of the task we interrupted
+	mov bx, [YKRdyList]
+	mov [bx], sp
+
+isr_keypress_not_lowest_interrupt:
+
+	call YKEnterISR
+
+		; Enable interrupts for higher-priority 
 	sti
-	
-	; run interrupt handler ? I think this is the c file
-	call c_tick_handler
-	
-	; disable interrupts ?
+
+		; Run interrupt handler
+	call c_isr_keypress
+
+
+		; disable interrupts
 	cli
+
+		;send EOI to PIC
+	mov	al, 0x20	; Load nonspecific EOI value (0x20) into register al
+	out	0x20, al	; Write EOI to PIC (port 0x20)
+
 	
-	; send eoi to pic
-	mov al, 0x20
-	out 0x20, al
-	
-	;restore reg
-	pop ds
+	call YKExitISR
+		; Restore context
 	pop es
-	pop bp
-	pop di	
+	pop ds
+	pop di
 	pop si
+	pop bp
+	pop dx
+	pop cx
+	pop bx
+	pop ax	
+		; Execute IRET
+	iret
+
+
+
+isr_tick:
+		; Save context
+	push ax
+	push bx
+	push cx
+	push dx
+	push bp
+	push si
+	push di
+	push ds
+	push es
+	
+	; Here we test to see if we are the lowest-level interrupt.
+	; If we are, we need to save the task's stack that we interrupted
+	mov ax, [YKISRCallDepth]
+	test ax, ax
+	jnz isr_tick_not_lowest_interrupt
+
+	; Save the SP of the task we interrupted
+	mov bx, [YKRdyList]
+	mov [bx], sp
+
+isr_tick_not_lowest_interrupt:
+
+	call YKEnterISR
+
+		; Enable interrupts for higher-priority 
+	sti
+
+		; Run interrupt handler
+	call c_isr_tick
+		; disable interrupts
+	cli
+
+		;send EOI to PIC
+	mov	al, 0x20	; Load nonspecific EOI value (0x20) into register al
+	out	0x20, al	; Write EOI to PIC (port 0x20)
+
+		
+	call YKExitISR
+		; Restore context
+	pop es
+	pop ds
+	pop di
+	pop si
+	pop bp
 	pop dx
 	pop cx
 	pop bx
 	pop ax
-	
-	; execute iret
-	iret 
+
+	   ; Execute IRET
+	iret
