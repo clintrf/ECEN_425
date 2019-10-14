676b65d69f25f5e444de8cf66bf60585d47c063c
Clint Frandsen <clint.frandsen@gmail.com>
2019-10-13 23:20:11 -0600

    Update yakc.c

--- lab04b/yakc.c
+++ lab04b/yakc.c
@@ -89,7 +89,10 @@ void YKNewTask( void (*task)(void), void *taskStack, unsigned char priority){   
     while (tmp2->priority < tmp->priority){
       tmp2 = tmp2->next;	/* assumes idle task is at end */
       printString("looking for Idle \n");
-      printf("%d , %d " , {tmp2->priority, tmp->priority})
+	    printInt((int) &(tmp2->priority));
+	    printString(" ");
+	    printInt((int) &(tmp->priority));
+	    printString("/n");
     }
     if (tmp2->prev == NULL)	/* insert in list before tmp2 */
       YKRdyList = tmp;
