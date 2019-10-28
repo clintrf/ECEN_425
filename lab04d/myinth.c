c7abb1d8c06aee40e486eddc35007f3ea007c37b
Clint Frandsen <clintfrandsen@Clints-Mac-Pro.byu.edu>
2019-10-28 11:17:58 -0600

    start lab4d

---
+++ lab04d/myinth.c
@@ -0,0 +1,51 @@
+#include "clib.h"
+#include "yakk.h"
+
+void delay();
+
+extern int KeyBuffer;
+
+#define DELAY 5000
+
+
+void c_reset_handler(){
+	printString("\nRESET PROGRAM\n");
+	exit(0);
+}
+
+
+void c_tick_handler(){
+	static unsigned int tick = 1;
+	printString("\nTICK ");
+	printInt(tick++);
+	printNewLine();
+	
+	YKTickHandler(); //lab4c
+}
+
+void c_key_handler(){
+	int i=0;
+	char c = (char) KeyBuffer;
+	if (c == 'd'){
+		printString("\r\nDELAY KEY PRESSED\r\n");
+		// delay();
+		for(i = 0; i < DELAY; i=i){
+			i++;
+		}
+		printString("\r\nDELAY COMPLETE$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\r\n");
+	}
+	else{
+		printString("\r\nKEYPRESS (");
+		printChar(c);
+		printString(") IGNORED*********************************************\r\n");
+	}
+}
+
+void delay(){
+	int i=0;
+	for(i = 0; i < DELAY; i++){}
+}
+
+void print_debug(){
+	printString("\r\nDEBUGGER\r\n");
+}
---
+++ lab04d/yaks.s
@@ -0,0 +1,99 @@
+;Assemble funtcions
+
+YKEnterMutex:                   ; Disables interrupts 
+	cli	                          ; clear interrupt flag
+	ret
+
+
+YKExitMutex:                    ; Enables Interrupts 
+	sti	                          ; set interrupt flag 
+	ret
+
+YKDispatcherNSave:   		; Dispatcher that saves to mem/stack
+	push bp
+	mov bp, sp
+
+	; Get the ss and sp values to restore and save them to ss and sp
+	mov sp, word[bp+4]			; getting first arg
+	mov ss, word[bp+6]			; getting second arg	
+	
+	mov	al, 0x20
+	out 	0x20, al
+	
+	; Pop register values off the stack
+	pop ES
+	pop DS
+	pop DI
+	pop SI
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	iret		
+
+YKDispatcherSave:   		; Dispatcher that saves to mem/stack
+	push bp
+	mov bp, sp
+	
+	;push AX
+	;mov BX, [bp+2]	; The return address.
+	pop bp
+	add sp, 2
+	
+	; Push flags, CS, and IP to the stack
+	pushf
+
+	; This is something about the flags that Ian told me to do
+	push BX
+	add sp, 2
+	pop BX
+	or BX, 0x200
+	push BX
+	sub sp, 2
+	pop BX
+	
+	push CS	
+	push word[bp+2]
+	; Push all register values to the stack
+	push AX
+	push BX
+	push CX
+	push DX
+	push BP						
+	push SI
+	push DI
+	push DS
+	push ES
+
+	; Move bp back to its original value
+	mov bp, sp
+	add bp, 20
+	
+	; Store the save sp to si
+	mov si, word [bp+4] 			; getting first arg (sp)
+	mov word [si], sp
+
+	; Store the save ss to si
+	mov si, word [bp+6]			; getting second arg (ss)
+	mov word [si], ss
+
+	; Save the sp and ss values to restore to sp and ss
+	mov sp, word[bp+8]			; getting third arg
+	mov ss, word[bp+10]			; getting fourth arg	
+	
+	mov	al, 0x20
+	out 	0x20, al
+
+	; Pop register values back off the stack
+	pop ES
+	pop DS
+	pop DI
+	pop SI
+	pop BP
+	pop DX
+	pop CX
+	pop BX
+	pop AX
+	iret			
+	
