7c51624535bab5539ab563e9d5365f69965e99a6
Clint Frandsen <clint.frandsen@gmail.com>
2019-10-28 11:31:05 -0600

    Update lab4d_app.c

--- lab04d/lab4d_app.c
+++ lab04d/lab4d_app.c
@@ -1,77 +1,51 @@
-/*
-File: lab4d_app.c
+/* 
+File: lab4c_app.c
 Revision date: 23 December 2003
-Description: Application code for EE 425 lab 4D (Kernel essentials D)
+Description: Application code for EE 425 lab 4C (Kernel essentials C)
 */
 
 #include "clib.h"
 #include "yakk.h"
 
-#define ASTACKSIZE 256          /* Size of task's stack in words */
-#define BSTACKSIZE 256
-#define CSTACKSIZE 256
-#define DSTACKSIZE 256
+#define STACKSIZE 256          /* Size of task's stack in words */
 
-int AStk[ASTACKSIZE];           /* Space for each task's stack  */
-int BStk[BSTACKSIZE];
-int CStk[CSTACKSIZE];
-int DStk[CSTACKSIZE];
+int TaskStack[STACKSIZE];      /* Space for task's stack */
 
-void ATask(void);               /* Function prototypes for task code */
-void BTask(void);
-void CTask(void);
-void DTask(void);
+void Task(void);               /* Function prototype for task code */
 
 void main(void)
 {
     YKInitialize();
-
-    printString("Creating tasks...\n");
-    YKNewTask(ATask, (void *) &AStk[ASTACKSIZE], 3);
-    YKNewTask(BTask, (void *) &BStk[BSTACKSIZE], 5);
-    YKNewTask(CTask, (void *) &CStk[CSTACKSIZE], 7);
-    YKNewTask(DTask, (void *) &DStk[DSTACKSIZE], 8);
+    
+    printString("Creating task...\n");
+    YKNewTask(Task, (void *) &TaskStack[STACKSIZE], 0);
 
     printString("Starting kernel...\n");
     YKRun();
 }
 
-void ATask(void)
+void Task(void)
 {
-    printString("Task A started.\n");
+    unsigned idleCount;
+    unsigned numCtxSwitches;
+
+    printString("Task started.\n");
     while (1)
     {
-        printString("Task A, delaying 2.\n");
+        printString("Delaying task...\n");
+
         YKDelayTask(2);
-    }
-}
 
-void BTask(void)
-{
-    printString("Task B started.\n");
-    while (1)
-    {
-        printString("Task B, delaying 3.\n");
-        YKDelayTask(3);
-    }
-}
+        YKEnterMutex();
+        numCtxSwitches = YKCtxSwCount;
+        idleCount = YKIdleCount;
+        YKIdleCount = 0;
+        YKExitMutex();
 
-void CTask(void)
-{
-    printString("Task C started.\n");
-    while (1)
-    {
-        printString("Task C, delaying 5.\n");
-        YKDelayTask(5);
-    }
-}
-
-void DTask(void)
-{
-    printString("Task D started.\n");
-    while (1)
-    {
-        printString("Task D, delaying 10.\n");
-        YKDelayTask(10);
+        printString("Task running after ");
+        printUInt(numCtxSwitches);
+        printString(" context switches! YKIdleCount is ");
+        printUInt(idleCount);
+        printString(".\n");
     }
 }
