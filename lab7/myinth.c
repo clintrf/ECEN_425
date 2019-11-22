62ed2dc4cccffe31a0e66c277aec1455fb74cd9c
Clint Frandsen <clint.frandsen@gmail.com>
2019-11-21 20:10:27 -0700

    Update myinth.c

--- lab7/myinth.c
+++ lab7/myinth.c
@@ -4,45 +4,17 @@
 
 
 extern int KeyBuffer;
-//extern YKSEM *NSemPtr; //lab05
-//void delay();
-//extern void YKTickHandler(void);
-//#define DELAY 5000
-
-//extern struct msg MsgArray[MSGARRAYSIZE];
-// extern YKQ *MsgQPtr;
-// extern int GlobalFlag;
 
 void c_reset_handler(){
 	exit(0);
 }
 
 void c_tick_handler(){
-    // static int next = 0;
-    // static int data = 0;
     YKTickHandler(); //lab4c
-
-    /* create a message with tick (sequence #) and pseudo-random data */
-    // MsgArray[next].tick = YKTickNum;
-    // data = (data + 89) % 100;
-    // MsgArray[next].data = data;
-
-    // if (YKQPost(MsgQPtr, (void *) &(MsgArray[next])) == 0){
-    //     printString("  TickISR: queue overflow! \n");
-    // }
-    // else if (++next >= MSGARRAYSIZE){
-    //     next = 0;
-    // }
-
-	// static int tick = 0;
-	// printString("\nTICK ");
-	// printInt(tick++);
-	// printNewLine();
-
 }
 
 void c_key_handler(){
-	char c;
+    char c;
     c = KeyBuffer;
 
     if(c == 'a') YKEventSet(charEvent, EVENT_A_KEY);
@@ -59,12 +31,3 @@ void c_key_handler(){
     }
 }
 
-/*
-void delay(){
-	int i=0;
-	for(i = 0; i < DELAY; i++){}
-}
-void print_debug(){
-	printString("\r\nDEBUGGER\r\n");
-}
-*/
